6ef0dfcf00b66ab2a96e87005bd86ff2d84b35df
lesterEkb <vakhitov.projects@yandex.ru>
2018-06-04 13:24:25 +0500

    1

--- library/mainFunctions.php
+++ library/mainFunctions.php
@@ -1,7 +1,18 @@
 <?
-
+/**
+  *
+  * Основные функции
+  *
+*/
+/**
+  * Формирование запрашиваемой страницы
+  *
+  * @param string $controllerName - название контролера
+  * @param string $actionName - название функции обработки страницы
+  *
+*/
 function loadPage($controllerName, $actionName = "index"){
-	include_once (PathPrefix . $controllerName . PathPostfix);
+	include_once PathPrefix . $controllerName . PathPostfix;
 
 	$function = $actionName . "Action";
 	$function();
