7e796c0337eaa028b92a76dec78e2b701a0c6bef
lesterEkb <vakhitov.projects@yandex.ru>
2018-06-04 14:36:36 +0500

    2

--- controllers/indexController.php
+++ controllers/indexController.php
@@ -8,4 +8,13 @@ function testAction(){
 	echo "IndexController.php > testAction";
 }
 
+/**
+    Формирование главной страницы
+ */
+function indexAction($smarty){
+    $smarty->assign('pageTitle', 'Главная страница сайта');
+
+    loadTemplaye($smarty, 'index');
+}
+
  ?>
